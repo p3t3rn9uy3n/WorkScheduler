d29ae85b957d315e355ab0241acc70caaa233b11
Peter Nguyen <p3t3rn9uy3n@gmail.com>
2021-03-13 22:50:06 -0600

    cleaned up style.css and script.js, fixed the javascript as something broke due to typo

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,51 +14,38 @@ var fivePm = $("#5pm");
 
 // Header date
 var interval = setInterval(function() {
-  var momentNow = moment();
+var momentNow = moment();
   $('#currentDay').html(momentNow.format('MMMM DD') + ' '
     + momentNow.format('dddd')
     .substring(0,3));
 }, 100);
 
 function timeStore() {
-  var time7 = JSON.parse(localStorage.getItem("07:00 am"));
+  var time7 = JSON.parse(localStorage.getItem("07:00 AM"));
   sevenAm.val(time7);
-  var time8 = JSON.parse(localStorage.getItem("8:00 am"));
+  var time8 = JSON.parse(localStorage.getItem("8:00 AM"));
   eightAm.val(time8);
-  var time9 = JSON.parse(localStorage.getItem("9:00 am"));
+  var time9 = JSON.parse(localStorage.getItem("9:00 AM"));
   nineAm.val(time9);
-  var time10 = JSON.parse(localStorage.getItem("10:00 am"));
+  var time10 = JSON.parse(localStorage.getItem("10:00 AM"));
   tenAm.val(time10);
-  var time11 = JSON.parse(localStorage.getItem("11:00 am"));
+  var time11 = JSON.parse(localStorage.getItem("11:00 AM"));
   elevenAm.val(time11);
-  var time12 = JSON.parse(localStorage.getItem("12:00 pm"));
+  var time12 = JSON.parse(localStorage.getItem("12:00 PM"));
   twelvePm.val(time12);
-  var time1 = JSON.parse(localStorage.getItem("1:00 pm"));
+  var time1 = JSON.parse(localStorage.getItem("1:00 PM"));
   onePm.val(time1);
-  var time2 = JSON.parse(localStorage.getItem("2:00 pm"));
+  var time2 = JSON.parse(localStorage.getItem("2:00 PM"));
   twoPm.val(time2);
-  var time3 = JSON.parse(localStorage.getItem("3:00 pm"));
+  var time3 = JSON.parse(localStorage.getItem("3:00 PM"));
   threePm.val(time3);
-  var time4 = JSON.parse(localStorage.getItem("4:00 pm"));
+  var time4 = JSON.parse(localStorage.getItem("4:00 PM"));
   fourPm.val(time4);
-  var time5 = JSON.parse(localStorage.getItem("5:00 pm"));
+  var time5 = JSON.parse(localStorage.getItem("5:00 PM"));
   fivePm.val(time5);
 } 
 
-$(document).ready(function(){
-    timeStore()
-    timeColor()
-  // Save button
-    $(".saveBtn").on("click", function(){
-      var userInput = $(this).siblings(".inputbox").val().trim();
-      var hourSpan = $(this).siblings(".input-group-prepend").text().trim();
-      localStorage.setItem(hourSpan, JSON.stringify(userInput));
-  
-    })
-});
-
-function timeColor () {
-      
+function background () {
   $(".inputbox").each(function () {
       var timeBack = parseInt($(this).attr("id"));
       hour = parseInt(hour);
@@ -71,3 +58,15 @@ function timeColor () {
       }
   });
 }
+
+$(document).ready(function(){
+  timeStore()
+  background()
+// Save button
+  $(".saveBtn").on("click", function(){
+    var input = $(this).siblings(".inputbox").val().trim();
+    var hourTime = $(this).siblings(".input-group-prepend").text().trim();
+    localStorage.setItem(hourTime, JSON.stringify(input));
+  })
+
+});
